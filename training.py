0405f0197580c2c5cead1db42498757897b6742f
fatshoy <ev.yakubovskiy@gmail.com>
2022-07-28 18:23:27 +0300

    init commit

---
+++ training.py
@@ -0,0 +1,13 @@
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    @property
+    def date(self):
+        return f'{self.day:02}/{self.month:02}/{self.year:04}'
+
+    @property
+    def usa_date(self):
+        return f'{self.month:02}-{self.day:02}-{self.year:04}'
